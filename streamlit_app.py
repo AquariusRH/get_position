a144c584a3b1723216f39d5fbc90d4ab6956399e
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-12-27 12:45:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ def get_race_map(race_no):
     return None
 
 # 設定頁面
-st.set_page_config(page_title="賽馬跑法與檔位分析器", layout="wide")
+st.set_page_config(page_title="賽馬跑法與檔位分析器_test", layout="wide")
 # --- 偵錯測試區 ---
-st.sidebar.subheader("🔍 API測試狀態")
+st.sidebar.subheader("🔍 API 測試狀態")
 test_race_no = (len(st.session_state.race_history) // 4) + 1
 test_url = f"https://racing.hkjc.com/racing/speedpro/assets/json/formguide/race_{test_race_no}.json"
 
